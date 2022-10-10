a189be9ab3ed7b15dfabe36553209f3c8e562e45
Guilherme Mattos da Silva <guilhermems.pp@gmail.com>
2022-10-09 22:30:19 -0300

    Atualizando a página Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,27 +1,91 @@
-import { memo, useEffect } from 'react'
+import { memo, useCallback, useEffect, useState } from 'react'
 
-import { useTranslation } from 'react-i18next'
+import { Col, Container, Row, Spinner } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
-import Config from 'Config'
+import BannerImage from 'assets/Banner.jpeg'
+import CharactersBanner from 'assets/characters.png'
+import ComicBanner from 'assets/comics.png'
 
-import LanguageSwitcher from 'components/LanguageSwitcher'
+import CharacterCard from 'components/CharacterCard'
+import Footer from 'components/Footer'
+import Header from 'components/Header'
 
-import useTitle from 'hooks/useTitle'
+import Api from 'services/Api'
+
+import { MainStyle } from 'styles/Main'
+
+import { CharacterType } from 'types/CharacterType'
 
 const Home: React.FC = () => {
-  const { t, i18n } = useTranslation()
-  const setTitle = useTitle()
+  const [characters, setCharacters] = useState<CharacterType[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const fetchCharacters = useCallback(async () => {
+    try {
+      const {
+        data: {
+          data: { results },
+        },
+      } = await Api.get('/characters')
+      setCharacters(results)
+    } catch {
+      ;<p>x</p>
+    } finally {
+      setIsLoading(false)
+    }
+  }, [])
 
   useEffect(() => {
-    setTitle(t('home.head-title'))
+    fetchCharacters()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [i18n.resolvedLanguage])
+  }, [])
 
   return (
     <>
-      <h1>{t('home.title')}</h1>
-      <LanguageSwitcher />
-      <p>{`v${Config.app.version}`}</p>
+      <Header />
+      <MainStyle>
+        <img
+          src={BannerImage}
+          alt="BannerImage"
+          style={{ width: `100%`, height: `auto` }}
+        />
+        <Container>
+          <Row>
+            <Col>
+              <Link to="/characters">
+                <img
+                  src={CharactersBanner}
+                  alt="CharactersBanner"
+                  style={{ width: `100%`, height: `auto` }}
+                />
+              </Link>
+              <Link to="/comics">
+                <img
+                  className="pt-3"
+                  src={ComicBanner}
+                  alt="ComicBanner"
+                  style={{ width: `100%`, height: `auto` }}
+                />
+              </Link>
+            </Col>
+          </Row>
+          <Row className="row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
+            {isLoading && (
+              <div className="text-center">
+                <Spinner animation="border" variant="success" />
+              </div>
+            )}
+            {!isLoading &&
+              characters.map((character) => (
+                <Col key={character.id} className="d-flex">
+                  <CharacterCard character={character} />
+                </Col>
+              ))}
+          </Row>
+        </Container>
+      </MainStyle>
+      <Footer />
     </>
   )
 }
