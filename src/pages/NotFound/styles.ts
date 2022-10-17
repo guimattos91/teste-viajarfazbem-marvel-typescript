f67b0fff7826303af6a972d1bf2e4df6692be944
Guilherme Mattos da Silva <guilhermems.pp@gmail.com>
2022-10-16 22:19:31 -0300

    Consertando nome da imagem "Hulk-Assets"

--- src/pages/NotFound/styles.ts
+++ src/pages/NotFound/styles.ts
@@ -21,30 +21,3 @@ export const TitleH1 = styled.h1`
   color: white;
   text-align: center;
 `
-
-export const InputStyle = styled.input`
-  border: none;
-  border-bottom: 2px solid red;
-  background-color: transparent;
-  color: #fff;
-  :focus {
-    box-shadow: 0 0 0 0;
-    border: 0 none;
-    border-bottom: 2px solid red;
-    outline: 0;
-  }
-`
-export const ButtonStyle = styled.button`
-  background-color: transparent;
-  border: none;
-  border-bottom: 2px solid #fa2d01;
-`
-export const ButtonBack = styled.button`
-  background-color: #fa2d01;
-  border: none;
-  text-align: center;
-`
-
-export const ColCards = styled(Col)`
-  flex: flex;
-`
